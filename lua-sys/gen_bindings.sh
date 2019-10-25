7c741bf06f128ee9e81cee62c83c9487d7a2df60
Yanis Guaye <yguaye44@gmail.com>
2019-10-25 15:00:44 +0200

    Improve bindgen script

--- lua-sys/gen_bindings.sh
+++ lua-sys/gen_bindings.sh
@@ -1,5 +1,14 @@
 # Generates the bindings using bindgen
-# Pass the input header as the first parameter
-# and the output file as the second one.
+# Pass the header paths as parameters.
 
-bindgen --ctypes-prefix libc --whitelist-function ^lua.* $1 -o $2
+# Example (windows):
+# ./gen_bindings.sh \
+#     "C:/Program Files (x86)/Windows Kits/10/Include/10.0.17763.0/ucrt" \
+#     "C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Tools/MSVC/14.15.26726/include" \
+#     "D:/Program Files/LLVM/lib/clang/9.0.0/include"
+
+bindgen \
+    --ctypes-prefix libc --whitelist-function ^lua.* \
+    wrapper.h -o lua_bindings.rs -- \
+    -Isrc/embedded \
+    "${@/#/-I}"
